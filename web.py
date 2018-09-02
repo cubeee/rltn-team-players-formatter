b257602ead2a4886492a0676f8fa6478632cdd6d
Cube <cubeee@users.noreply.github.com>
2018-09-03 01:13:38 +0300

    Add parentheses to print

--- web.py
+++ web.py
@@ -68,7 +68,7 @@ class Handler(BaseHTTPRequestHandler):
 def run(server_class=HTTPServer, handler_class=Handler, port=80):
     server_address = ('', port)
     httpd = server_class(server_address, handler_class)
-    print 'Starting httpd...'
+    print('Starting httpd...')
     httpd.serve_forever()
 
 if __name__ == "__main__":
